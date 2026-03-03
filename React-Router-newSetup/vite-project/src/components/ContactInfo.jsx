43f8ad65ab15051719a37ab55514bf230e2dd229
indranil7863 <ibera866@gmail.com>
2026-03-03 21:41:57 +0530

    change dir name

---
+++ React-Router-newSetup/vite-project/src/components/ContactInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div>
+      <p>Name: Indranil bera</p>
+      <p>Email: example@gmail.com</p>
+      <p>address: xyz street, kolkata</p>
+    </div>
+  );
+};
+
+export default ContactInfo;
